822a9664953aff7f25afd10745f99c101ce19d23
AisyahFitriani <afitriani441@gmail.com>
2023-09-19 17:18:18 +0700

    menambahkan library

---
+++ colors.js
@@ -0,0 +1,11 @@
+var colors = require('colors');
+
+console.log('hello' .yellow);
+
+console.log('i like fruits' .underline.blue);
+
+console.log('inverse the color' .inverse);
+
+console.log('OMG Rainbows!' .rainbow);
+
+console.log('Run the trap'.trap);
